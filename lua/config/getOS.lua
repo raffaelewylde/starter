216e8ce069e277c5b0483dd675d33ad5d2fe363b
Raffaele Wylde <75472685+raffaelewylde@users.noreply.github.com>
2024-07-27 22:57:08 -0400

    added workign getOS function I think

---
+++ lua/config/getOS.lua
@@ -0,0 +1,13 @@
+function getOS()
+	if jit then
+		return jit.os
+	end
+
+	local fh, err = assert(io.popen("uname -o 2>/dev/null", "r"))
+	if fh then
+		osname = fh:read()
+	end
+
+	return osname or "Windows"
+end
+print(getOS())
