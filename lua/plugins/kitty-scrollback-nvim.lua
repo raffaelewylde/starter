67728fec4f0fd030de5c54107754e70281b2a91d
Raffaele Wylde <75472685+raffaelewylde@users.noreply.github.com>
2025-05-13 19:21:38 -0400

    changed colorscheme for kitty-scrollback-nvim

--- lua/plugins/kitty-scrollback-nvim.lua
+++ lua/plugins/kitty-scrollback-nvim.lua
@@ -1,12 +1,12 @@
 local kitty_scrollback_config = function()
-  require('kitty-scrollback').setup({
+  require("kitty-scrollback").setup({
     -- global config
     {
-      visual_selection_highlight_mode = 'reverse',
+      visual_selection_highlight_mode = "reverse",
       callbacks = {
         after_ready = function()
           vim.defer_fn(function()
-            vim.notify('\nThis is a global config', vim.log.levels.WARN, {})
+            vim.notify("\nThis is a global config", vim.log.levels.WARN, {})
           end, 1000)
         end,
       },
@@ -15,7 +15,7 @@ local kitty_scrollback_config = function()
       callbacks = {
         after_ready = function()
           vim.defer_fn(function()
-            vim.api.nvim_feedkeys('?', 'n', false)
+            vim.api.nvim_feedkeys("?", "n", false)
           end, 100)
         end,
       },
@@ -23,23 +23,27 @@ local kitty_scrollback_config = function()
   })
 end
 
-if vim.env.KITTY_SCROLLBACK_NVIM == 'true' then
+if vim.env.KITTY_SCROLLBACK_NVIM == "true" then
   -- kitty-scrollback.nvim minimal config
-  vim.opt.runtimepath:append(vim.fn.stdpath('data') .. '/lazy/kitty-scrollback.nvim') -- lazy.nvim
-  vim.g.mapleader = ' '
-  vim.g.maplocalleader = ','
-  vim.cmd.colorscheme('zaibatsu')
+  vim.opt.runtimepath:append(vim.fn.stdpath("data") .. "/lazy/kitty-scrollback.nvim") -- lazy.nvim
+  vim.g.mapleader = " "
+  vim.g.maplocalleader = ","
+  vim.cmd.colorscheme("cyberdream")
   kitty_scrollback_config()
   return {}
 else
   -- kitty-scrollback.nvim lazy.nvim config
   return {
     {
-      'mikesmithgh/kitty-scrollback.nvim',
+      "mikesmithgh/kitty-scrollback.nvim",
       enabled = true,
       lazy = true,
-      cmd = { 'KittyScrollbackGenerateKittens', 'KittyScrollbackCheckHealth' },
-      event = { 'User KittyScrollbackLaunch' },
+      cmd = {
+        "KittyScrollbackGenerateKittens",
+        "KittyScrollbackCheckHealth",
+        "KittyScrollbackGenerateCommandLineEditing",
+      },
+      event = { "User KittyScrollbackLaunch" },
       config = kitty_scrollback_config,
     },
   }
